73e1fd7e1ebaa4b69693cfe40f7089001562fa45
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-11 20:16:18 +0330

    feat: added names for endpoints for convenience

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import Balance, Charge
 
 urlpatterns = [
-    path("charge/", Charge.as_view()),
-    path("balance/", Balance.as_view()),
+    path("charge/", Charge.as_view(), name="charge"),
+    path("balance/", Balance.as_view(), name="balance"),
 ]
