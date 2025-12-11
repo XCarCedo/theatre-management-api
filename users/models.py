f966cf4c77127b99e81a40ccd843d7c6553b7c27
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-11 15:49:55 +0330

    feat: implemented a new user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from decimal import Decimal
+from random import randint
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        customer = ("customer", "Customer")
+        manager = ("manager", "Manager")
+
+    full_name = models.CharField(default=f"user_{randint(100_000_000, 999_999_999)}")
+    role = models.CharField(max_length=8, choices=Role.choices, default=Role.customer)
+    balance = models.DecimalField(
+        decimal_places=2, default=Decimal("0.00"), max_digits=10
+    )
