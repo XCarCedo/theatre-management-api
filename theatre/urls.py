6aeab53462c08a05007cc3c7da4a82b3c9534432
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-11 20:34:46 +0330

    feat: added dj_rest_auth endpoints and browsable api endpoints

--- theatre/urls.py
+++ theatre/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/browsable-api-auth/", include("rest_framework.urls")),
     path("api/v1/my/", include("users.urls")),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
 ]
