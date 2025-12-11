a1f418a4007cd7a0c346fea9dd688d73d707bc64
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-11 16:09:10 +0330

    feat: added a new custom user admin with the appropriate forms

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    list_display = ("username", "full_name", "role", "balance", "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("full_name", "email", "role", "balance")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "full_name",
+                    "email",
+                    "role",
+                    "balance",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+    search_fields = ("username", "full_name", "email")
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
