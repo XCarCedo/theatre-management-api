32a27ccdd14c11c8258f4b1c493c5bba1a028d4f
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-11 20:38:05 +0330

    feat: added registration endpoints

--- theatre/urls.py
+++ theatre/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("api/v1/browsable-api-auth/", include("rest_framework.urls")),
     path("api/v1/my/", include("users.urls")),
     path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/v1/auth/registration", include("dj_rest_auth.registration.urls")),
 ]
