12417e1c3a1843649691d7883712f9f95ad4832f
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-13 20:31:34 +0330

    feat: added views for detail and list view of theatre model

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Theatre
+from .permissions import AdminWriteUserReadPermission
+from .serializers import TheatreSerializer
+
+
+class TheatreListView(generics.ListCreateAPIView):
+    queryset = Theatre.objects.all()
+    serializer_class = TheatreSerializer
+    permission_classes = [AdminWriteUserReadPermission]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+class TheatreDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Theatre.objects.all()
+    serializer_class = TheatreSerializer
+    permission_classes = [AdminWriteUserReadPermission]
