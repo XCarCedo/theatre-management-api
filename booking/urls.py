01909c7b0bdb8ba23cc894a0ddd2a13b44deb80e
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-13 20:32:17 +0330

    chore: routed endpoints to the corresponding detail/list view

---
+++ booking/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.TheatreListView.as_view(), name="theatre_list"),
+    path("<int:pk>/", views.TheatreDetailView.as_view(), name="theatre_detail"),
+]
