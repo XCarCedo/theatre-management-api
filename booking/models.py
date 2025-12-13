b58b534935877e77955a01c5c0b7818f99fc4d1a
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-13 13:19:56 +0330

    feat: initialized theatre/seat models

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Theatre(models.Model):
+    name = models.CharField(max_length=300)
+    seats_count = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
+
+
+class Seat(models.Model):
+    theatre = models.ForeignKey(Theatre, on_delete=models.CASCADE)
+    occupied_by = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE
+    )
+    number = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
