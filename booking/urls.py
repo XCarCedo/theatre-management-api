1d40eae33c1fabf8589204dd1a2e9f3417813aa8
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-14 21:56:11 +0330

    chore: routed seat reserve endpoint to corresponding view

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.TheatreListView.as_view(), name="theatre_list"),
     path("<int:pk>/", views.TheatreDetailView.as_view(), name="theatre_detail"),
+    path("reserve/<int:pk>/", views.SeatReserveView.as_view(), name="reserve_seat"),
 ]
