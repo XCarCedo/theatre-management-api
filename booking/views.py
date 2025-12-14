bed5d6b840f9afa68f97c0ade3f314bb690a5f6d
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-14 21:55:46 +0330

    feat: added endpoints for seat reserve

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,8 @@
-from rest_framework import generics
+from django.db.models import F
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status, views
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Seat, Theatre
 from .permissions import AdminWriteUserReadPermission
@@ -24,3 +28,28 @@ class TheatreDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Theatre.objects.all()
     serializer_class = TheatreDetailSerializer
     permission_classes = [AdminWriteUserReadPermission]
+
+
+class SeatReserveView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        seat = get_object_or_404(Seat, pk=pk)
+
+        if seat.occupied_by is not None:
+            return Response(
+                {"ok": False, "message": "Seat already occupied"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if not request.user.balance >= seat.price:
+            return Response(
+                {"ok": False, "message": "Balance is not enough"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        request.user.balance = F("balance") - seat.price
+        seat.occupied_by = request.user
+        seat.save()
+
+        return Response({"ok": True, "message": "Seat reserved"})
