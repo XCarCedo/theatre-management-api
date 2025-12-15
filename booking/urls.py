b9800bb06c06fbaaf30bc315ba0aab65ea3ca66c
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-15 22:26:35 +0330

    chore: changed reserve seat endpoint

--- booking/urls.py
+++ booking/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.TheatreListView.as_view(), name="theatre_list"),
     path("<int:pk>/", views.TheatreDetailView.as_view(), name="theatre_detail"),
-    path("reserve/<int:pk>/", views.SeatReserveView.as_view(), name="reserve_seat"),
+    path(
+        "seat/reserve/<int:pk>/", views.SeatReserveView.as_view(), name="reserve_seat"
+    ),
 ]
