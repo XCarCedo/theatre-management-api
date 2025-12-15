62479d33d3c91e6f8510ff52e753571f2621cdba
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2025-12-15 20:12:41 +0330

    fix: added authentication before role check in custom permission

--- booking/permissions.py
+++ booking/permissions.py
@@ -4,19 +4,19 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class AdminWriteUserReadPermission(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
         if request.method in SAFE_METHODS:
             return True
-
-        if type(request.user) is AnonymousUser:
-            return False
 
         return request.user.is_superuser or request.user.role == "manager"
 
     def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
+
         if request.method in SAFE_METHODS:
             return True
-
-        if type(request.user) is AnonymousUser:
-            return False
 
         return request.user.is_superuser or request.user.role == "manager"
